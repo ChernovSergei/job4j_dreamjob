0c4788572b6893a251ee1f6b5b09e91b879d7cf1
ChernovSergei <goldengrail@gmail.com>
2026-02-04 11:45:05 +0500

    vacancies corrections

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -1,15 +1,21 @@
 package ru.job4j.dreamjob.model;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Vacancy {
     private int id;
     private String title;
+    private String description;
+    private LocalDateTime creationDate;
 
-    public Vacancy(int id, String title) {
+    public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
         this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
     }
+
     public int getId() {
         return id;
     }
@@ -26,15 +32,36 @@ public class Vacancy {
         this.title = title;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
     @Override
     public boolean equals(Object object) {
-        if (object == null || getClass() != object.getClass()) return false;
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
         Vacancy vacancy = (Vacancy) object;
-        return id == vacancy.id && Objects.equals(title, vacancy.title);
+        return id == vacancy.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title);
+        return Objects.hash(id);
     }
 }
--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -2,9 +2,10 @@ package ru.job4j.dreamjob.repository;
 
 import ru.job4j.dreamjob.model.Vacancy;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
-public class MemoryVacancyRepository implements VacancyRepository{
+public class MemoryVacancyRepository implements VacancyRepository {
 
     private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
     private int nextId = 1;
@@ -12,12 +13,24 @@ public class MemoryVacancyRepository implements VacancyRepository{
     private final Map<Integer, Vacancy> vacancies = new HashMap<>();
 
     public MemoryVacancyRepository() {
-        save(new Vacancy(0, "Intern Java Developer"));
-        save(new Vacancy(0, "Junior Java Developer"));
-        save(new Vacancy(0, "Junior+ Java Developer"));
-        save(new Vacancy(0, "Middle Java Developer"));
-        save(new Vacancy(0, "Middle+ Java Developer"));
-        save(new Vacancy(0, "Senior Java Developer"));
+        save(new Vacancy(0, "Intern Java Developer",
+                "Learn how to press keys for money",
+                LocalDateTime.of(2023, 01, 05, 12, 36)));
+        save(new Vacancy(0, "Junior Java Developer",
+                "Learn how to program for money",
+                LocalDateTime.of(2000, 3, 28, 16, 5)));
+        save(new Vacancy(0, "Junior+ Java Developer",
+                "Here you are",
+                LocalDateTime.of(2013, 9, 9, 9, 30)));
+        save(new Vacancy(0, "Middle Java Developer",
+                "You will implement some projects in a team ",
+                LocalDateTime.of(2007, 11, 1, 22, 0)));
+        save(new Vacancy(0, "Middle+ Java Developer",
+                "You will implement your job + senior job without additional payment",
+                LocalDateTime.of(2009, 10, 22, 22, 22)));
+        save(new Vacancy(0, "Senior Java Developer",
+                "You will be alone in the building and making money",
+                LocalDateTime.of(1996, 8, 11, 9, 30)));
     }
 
     public static MemoryVacancyRepository getInstance() {
@@ -39,7 +52,11 @@ public class MemoryVacancyRepository implements VacancyRepository{
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(),
-                (id, oldVacancy) -> new Vacancy(oldVacancy.getId(), vacancy.getTitle())) != null;
+                (id, oldVacancy) ->
+                        new Vacancy(oldVacancy.getId(),
+                                vacancy.getTitle(),
+                                vacancy.getDescription(),
+                                vacancy.getCreationDate())) != null;
     }
 
     @Override
