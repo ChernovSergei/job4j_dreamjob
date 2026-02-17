28f71c32885d89f7f26ebc715cfae986e301d853
ChernovSergei <goldengrail@gmail.com>
2026-02-17 20:39:17 +0500

    Vacancy publish interface had been added

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -20,22 +20,28 @@ public class MemoryVacancyRepository implements VacancyRepository {
     public MemoryVacancyRepository() {
         save(new Vacancy(0, "Intern Java Developer",
                 "Learn how to press keys for money",
-                LocalDateTime.of(2023, 1, 5, 12, 36)));
+                LocalDateTime.of(2023, 1, 5, 12, 36),
+                true));
         save(new Vacancy(0, "Junior Java Developer",
                 "Learn how to program for money",
-                LocalDateTime.of(2000, 3, 28, 16, 5)));
+                LocalDateTime.of(2000, 3, 28, 16, 5),
+                true));
         save(new Vacancy(0, "Junior+ Java Developer",
                 "Here you are",
-                LocalDateTime.of(2013, 9, 9, 9, 30)));
+                LocalDateTime.of(2013, 9, 9, 9, 30),
+                true));
         save(new Vacancy(0, "Middle Java Developer",
                 "You will implement some projects in a team ",
-                LocalDateTime.of(2007, 11, 1, 22, 0)));
+                LocalDateTime.of(2007, 11, 1, 22, 0),
+                true));
         save(new Vacancy(0, "Middle+ Java Developer",
                 "You will implement your job + senior job without additional payment",
-                LocalDateTime.of(2009, 10, 22, 22, 22)));
+                LocalDateTime.of(2009, 10, 22, 22, 22),
+                true));
         save(new Vacancy(0, "Senior Java Developer",
                 "You will be alone in the building and making money",
-                LocalDateTime.of(1996, 8, 11, 9, 30)));
+                LocalDateTime.of(1996, 8, 11, 9, 30),
+                true));
     }
 
     @Override
@@ -57,7 +63,8 @@ public class MemoryVacancyRepository implements VacancyRepository {
                         new Vacancy(oldVacancy.getId(),
                                 vacancy.getTitle(),
                                 vacancy.getDescription(),
-                                vacancy.getCreationDate())) != null;
+                                vacancy.getCreationDate(),
+                                vacancy.getVisible())) != null;
     }
 
     @Override
