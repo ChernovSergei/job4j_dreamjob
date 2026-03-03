341f022e19f21ad5e78d55041c171d403850dd46
ChernovSergei <goldengrail@gmail.com>
2026-03-03 21:46:04 +0500

    user register form and repo were added

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,37 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.dreamjob.dto.FileDto;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.service.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage() {
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user, Model model) {
+        try {
+            userService.save(user);
+            return "redirect:/users";
+        } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
+            return "errors/404";
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/dreamjob/model/User.java
@@ -0,0 +1,67 @@
+package ru.job4j.dreamjob.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String email;
+    private String name;
+    private String password;
+
+    public User(int id, String email, String name, String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        User user = (User) object;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+}
