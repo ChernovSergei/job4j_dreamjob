e9de5cff2b2c7bb3b3a0ea2e1f03c7ad951308e2
Sergei <goldengrail@gmail.com>
2026-03-16 13:19:38 +0500

    HttpSession initial changes

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,13 +1,23 @@
 package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+import ru.job4j.dreamjob.model.User;
 
 @Controller
 @SuppressWarnings("unused")
 public class IndexController {
-    @GetMapping("/index")
-    public String getIndex() {
+    @GetMapping({"/", "/index"})
+    public String getIndex(Model model, HttpSession session) {
+        var user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setName("Guest");
+        }
+        model.addAttribute("user", user);
         return "index";
     }
 }
--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.UserService;
 
@@ -43,12 +46,20 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String loginUser(@ModelAttribute User user, Model model) {
+    public String loginUser(@ModelAttribute User user, Model model, HttpServletRequest request) {
         var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
         if (userOptional.isEmpty()) {
             model.addAttribute("error", "Email or password are not correct");
             return "users/login";
         }
+        var session = request.getSession();
+        session.setAttribute("user", userOptional.get());
         return "redirect:/vacancies";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/users/login";
     }
 }
