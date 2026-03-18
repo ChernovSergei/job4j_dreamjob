dcabd4969b54c70d2ee096306baa86b3cd0ac57c
Sergei <goldengrail@gmail.com>
2026-03-18 12:57:33 +0500

    Authorization Filter had been added

---
+++ src/main/java/ru/job4j/dreamjob/filter/SessionFilter.java
@@ -0,0 +1,34 @@
+package ru.job4j.dreamjob.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import ru.job4j.dreamjob.model.User;
+
+@Component
+@Order(2)
+public class SessionFilter extends HttpFilter {
+    
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        var session = request.getSession();
+        addUserToSession(session, request);
+        chain.doFilter(request, response);
+    }
+
+    private void addUserToSession(HttpSession session, HttpServletRequest request) {
+        var user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setName("Guest");
+        }
+        request.setAttribute("user", user);
+    }
+}
